e979edacb7a556761234eaa01e9d6b2668fe09d6
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2025-06-02 06:39:26 -0400

    Page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,14 @@
 	import { page } from '$app/stores';
 	import Nav from '$lib/components/Nav.svelte';
 	const PUBLIC_UMAMI_WEBSITE_ID = import.meta.env.PUBLIC_UMAMI_WEBSITE_ID;
+
+	$: pageTitle = $page.url.pathname === '/' 
+		? 'Robert Boskind Photography'
+		: `Robert Boskind Photography - ${$page.url.pathname.slice(1).split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}`;
 </script>
 
 <svelte:head>
+	<title>{pageTitle}</title>
 	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 	{#if PUBLIC_UMAMI_WEBSITE_ID}
 		<script
